b050377878994d215995ad4e63a400641670f415
Jesus017x <matos.alonso12@gmail.com>
2022-06-24 21:13:47 -0400

    built a simple API

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.0</p>')
+})
+
+app.get('/products', (req, res) => {
+    res.send([{
+        productId: '131',
+        price: 100
+    },
+    {
+        productId: '222',
+        price: 125
+    }])
+})
+
+app.listen(port, ()=> {
+    console.log(`Demo app is up and listening to port: ${port}`);
+})
